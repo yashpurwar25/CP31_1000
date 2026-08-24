cea6e256164f82bbc9b9c568d7111ecad9a51598
yashpurwar25 <yashpurwar244@gmail.com>
2026-08-25 02:45:46 +0530

    Refactor prefix and suffix calculations in 1791D.cpp

--- 1791D.cpp
+++ 1791D.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
     while(t--){
@@ -9,23 +12,27 @@ int main(){
         cin >> n;
         string s;
         cin >> s;
-        unordered_set<char> st;
-        vector<long long> prefix(n+1,0);
-        vector<long long> suffix(n+1,0);
-        for(long long i=1;i<=n;i++){
-            st.insert(s[i-1]);
-            prefix[i]=st.size();
+
+        vector<int> prefix(n+1, 0);
+        vector<int> suffix(n+2, 0);
+
+        unordered_set<char> str;
+        for(long long i = 1; i <= n; i++){
+            str.insert(s[i-1]);
+            prefix[i] = str.size();
         }
-        st.clear();
-        for(long long i=n;i>0;i++){
-            st.insert(s[i-1]);
-            suffix[i]=st.size();
+
+        str.clear();
+        for(long long i = n; i >= 1; i--){
+            str.insert(s[i-1]);
+            suffix[i] = str.size();
         }
-        long long ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,prefix[i]+suffix[i+1]);
-            cout << ans << endl;
+
+        long long ans = 0;
+        for(long long k = 1; k < n; k++){
+            ans = max(ans, (long long)prefix[k] + suffix[k+1]);
         }
+        cout << ans << "\n";
     }
     return 0;
 }
