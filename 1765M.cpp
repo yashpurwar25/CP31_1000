c79a9f6dfbd06212d7f417f11e5fb28ee76efcaf
yashpurwar25 <yashpurwar244@gmail.com>
2026-08-28 01:40:51 +0530

    Update 1765M.cpp

--- 1765M.cpp
+++ 1765M.cpp
@@ -6,13 +6,15 @@ int main(){
     while(t--){
         long long n;
         cin >> n;
-        long long ans_a=1,ans_b=n-1;
-        for(long long i=0;i*i<n;i++){
+        long long ans_a=1;
+        long long ans_b=n-1;
+        for(long long i=2;i*i<=n;i++){
             if(n%i==0){
                 ans_a=n/i;
-                ans_b=i;
+                ans_b=n-ans_a;
+                break;
             }
         }
-        cout << ans_a << " " << ans_b;
+        cout << ans_a << " " << ans_b << endl;
     }
 }
