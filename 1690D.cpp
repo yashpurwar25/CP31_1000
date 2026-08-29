235bf16f7cae22f269bbc12a0e29320bb43a1e5d
yashpurwar25 <yashpurwar244@gmail.com>
2026-08-29 08:06:28 +0530

    Create 1690D.cpp

---
+++ 1690D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        string s; 
+        cin >> s; // n
+        vector<long long> prefix(n + 1, 0);
+        for (int i = 0; i < n; i++) // n
+            prefix[i + 1] = prefix[i] + (s[i] == 'W');
+ 
+        long long minimum_cells = INT_MAX;
+        for (int i = 0; i <= n - k; i++) // n
+        {
+            long long diff = prefix[i + k] - prefix[i];
+            minimum_cells = min(minimum_cells, diff);
+        }
+ 
+        cout << minimum_cells << endl;
+    }
+    return 0;
+}
