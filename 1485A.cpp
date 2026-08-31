bd334e24ad6a7f244ed009112a02f00be9691033
yashpurwar25 <yashpurwar244@gmail.com>
2026-09-01 00:48:48 +0530

    Create 1485A.cpp

---
+++ 1485A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b;
+        cin >> a >> b;
+ 
+        long long ans = INT_MAX;
+        for (int addition = 0; addition < 32; addition++) // log2(a)
+        {
+            long long operations = addition;
+            long long new_b = b + addition;
+            if (new_b == 1) // corner
+                continue;
+            long long copy_a = a;
+            while (copy_a > 0) // log2(a)
+            {
+                copy_a /= new_b;
+                operations++;
+            }
+            ans = min(ans, operations);
+        }
+        cout << ans << endl;
+    }
+}
+//observe pattern like when a = 10e9 and b =1 and op when we increase addition and once when we increase division
